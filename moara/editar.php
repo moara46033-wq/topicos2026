60a55385b43f32dee990ad20f01b8c3f27bbe3c5
moara46033-wq <moara.46033@aluno.iffar.edu.br>
2026-04-01 20:11:35 -0300

    Update editar.php

--- moara/editar.php
+++ moara/editar.php
@@ -1,44 +1,61 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Editar Contato</title>
+    <link rel="stylesheet" href="estilo.css">
+</head>
+<body>
+<div class='forms'>
 <?php
-include ('conexao.php');
+include('conexao.php');
 
-$id=$_GET['id'];
+$id = $_GET['id'];
 
-$sql= "SELECT * FROM contatos WHERE id= $id";
+$sql = "SELECT * FROM contatos WHERE id = $id";
 
-//o id não precisa de aspas porque no banco os unicos que precisam de aspas é os que sao varchar//
+$resultado = mysqli_query($conexao, $sql);
 
-$resultado= mysqli_query($conexao,$sql);
-
-if(mysqli_num_rows($resultado) == 1){
-    $contato= mysqli_fetch_assoc($resultado);
-}else{
-    echo "Contato não encontrato na base.";
+if (mysqli_num_rows($resultado) == 1) {
+    $contato = mysqli_fetch_assoc($resultado);
+} else {
+    echo "<h2>Contato não encontrado na base.</h2>";
+    echo "<a href='index.php'>Voltar</a>";
     exit;
 }
 
-if (isset($_POST ['atualizar'])){
-    $novo_nome=$_POST ['nome'];
-    $novo_endereco=$_POST ['endereco'];
-    $novo_fone=$_POST ['fone'];
+if (isset($_POST['atualizar'])) {
+    $novo_nome = $_POST['nome'];
+    $novo_endereco = $_POST['endereco'];
+    $novo_fone = $_POST['fone'];
 
-    $sql2= "UPDATE contatos SET nome= '$novo_nome',
-    endereco= '$novo_endereco', telefone = '$novo_fone'
-    WHERE id=$id";
+    $sql2 = "UPDATE contatos SET 
+                nome = '$novo_nome',
+                endereco = '$novo_endereco', 
+                telefone = '$novo_fone'
+             WHERE id = $id";
 
-    if (mysqli_query($conexao,$sql2)){
-        echo "<h2> Contato atualizado com sucesso</h2>";
-        echo "<a href='index.php'> VOLTAR</a>";
-    } else{
+    if (mysqli_query($conexao, $sql2)) {
+        echo "<h2>Contato atualizado com sucesso</h2>";
+        echo "<a href='lista.php'>Voltar para a lista de contatos</a>";
+    } else {
         echo "<h2>Erro ao atualizar.</h2>" . mysqli_error($conexao);
-        echo "<a href='index.php'>VOLTAR</a>";
+        echo "<a href='lista.php'>Voltar para a lista de contatos</a>";
     }
 }
 ?>
 
 <form method="post">
-    Nome <input type="text" name="nome" value="<?php echo $contato ['nome'];?>"> <br>
-    Endereco: <input type="text" name="endereco" value="<?php echo $contato['endereco'];?>"> <br>
-   Telefone: <input type="text" name="fone" value="<?php echo $contato['telefone'];?>"> <br> 
-     <input type="submit" name="atualizar" value="Atualizar">
-
+    Nome: <input type="text" name="nome" value="<?php echo $contato['nome']; ?>"> <br>
+    Endereço: <input type="text" name="endereco" value="<?php echo $contato['endereco']; ?>"> <br>
+    <p>Telefone</p>
+        <input type="text" name="fone" id="telefone" placeholder="(99) 99999-9999" maxlength="15"
+       oninput="this.value = this.value.replace(/\D/g,'')
+                   .replace(/^(\d{2})(\d)/,'($1) $2')
+                   .replace(/(\d{5})(\d)/,'$1-$2');" value="<?php echo $contato['telefone']; ?>"> <br>
+    <input type="submit" name="atualizar" value="Atualizar">
 </form>
+</div>
+
+</body>
+</html>
